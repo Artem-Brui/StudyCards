26fc0414de698f9969db1a7d04de685fae82682a
Artem-Brui <artembryj@gmail.com>
2025-12-26 19:21:01 +0100

    add card item to study mode

--- src/components/CardItem/CardItem.tsx
+++ src/components/CardItem/CardItem.tsx
@@ -1,6 +1,7 @@
 import type React from "react";
 import styles from "./CardItem.module.scss";
 import type { CardItemType } from "@/assets/base/base";
+import { useState } from "react";
 
 export type CardVersionType = "list" | "study";
 
@@ -13,16 +14,30 @@ export const CardItem: React.FC<СardComponentProps> = ({
   cardVersion,
   card,
 }) => {
+  const [answerSwitcher, setAnswerSwitcher] = useState(false);
+
   const { question, answer, category } = card;
-  const isStudy = cardVersion === 'study';
+  const isListPage = cardVersion === "list";
+  const isQuestionVisibile = isListPage || !answerSwitcher;
+  const isAnswerVisibile = isListPage || answerSwitcher;
+
+  const handleCardClick = () => {
+    if (isListPage) return;
+
+    setAnswerSwitcher((x) => !x);
+  };
 
   return (
-    <article className={styles.card_item} data-version={cardVersion}>
-      <p className={styles.question}>{question}</p>
-      <p className={styles.answer}>{answer}</p>
+    <article
+      className={styles.card_item}
+      data-version={cardVersion}
+      onClick={handleCardClick}
+    >
+      {isQuestionVisibile && <p className={styles.question}>{question}</p>}
+      {isAnswerVisibile && <p className={styles.answer}>{answer}</p>}
       <p className={styles.category}>{category}</p>
       <p className={styles.progress}>Progress</p>
-      {!isStudy && <button className={styles.service_button}></button>}
+      {isListPage && <button className={styles.service_button}></button>}
     </article>
   );
 };
--- src/pages/StudyModePage/StudyModePage.tsx
+++ src/pages/StudyModePage/StudyModePage.tsx
@@ -1,11 +1,17 @@
+import { CardItem } from "@/components/CardItem/CardItem";
 import { Header } from "@/components/Header/Header";
+import base from "@/assets/base/base.json";
+import type { CardItemType } from "@/assets/base/base";
+
+const cards: CardItemType[] = base;
+
 
 function StudyModePage() {
   return (
     <>
       <Header />
       <main>
-        <p>Study Mode</p>
+        <CardItem cardVersion="study" card={cards[5]} />
       </main>
     </>
   );
